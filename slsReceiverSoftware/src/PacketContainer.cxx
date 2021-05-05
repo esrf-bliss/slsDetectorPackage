379e84b71e649845d788d1d03e5f4481a8bf3a05
Alejandro Homs Puron <ahoms@esrf.fr>
2021-05-05 12:41:15 +0200

    slsReceiver: CamelCase rename PacketContainer::cleanUp

--- slsReceiverSoftware/src/PacketContainer.cxx
+++ slsReceiverSoftware/src/PacketContainer.cxx
@@ -33,7 +33,7 @@ PacketContainer<P>::PacketContainer(int frames, unsigned long node_mask,
 
 template <class P> PacketContainer<P>::~PacketContainer() {
     stop();
-    cleanup();
+    cleanUp();
 }
 
 template <class P>
@@ -158,7 +158,7 @@ template <class P> void PacketContainer<P>::stop() {
     }
 }
 
-template <class P> void PacketContainer<P>::cleanup() {
+template <class P> void PacketContainer<P>::cleanUp() {
     releaseReadyPacketBlocks();
     waitUsedPacketBlocks();
 }
