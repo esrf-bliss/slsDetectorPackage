2030bc973c88c8c940bc249e8bfd9558b85aa291
Alejandro Homs Puron <ahoms@esrf.fr>
2024-08-31 00:21:37 +0200

    slsReceiver: force sequential frame readout; improve reliability on data loss

--- slsSupportLib/include/sls/StreamData.h
+++ slsSupportLib/include/sls/StreamData.h
@@ -10,8 +10,12 @@ namespace sls {
 // PacketStream
 template <class Packet> struct StreamData {
     // Describes the sequence of the packets in the stream
-    std::uint32_t getPacketNumber(std::uint32_t packet_idx) {
+    constexpr std::uint32_t getPacketNumber(std::uint32_t packet_idx) {
         return packet_idx;
+    }
+    // Reverse sequence of the packets in the stream
+    constexpr std::uint32_t getPacketIndex(std::uint32_t packet_nb) {
+        return packet_nb;
     }
 };
 
